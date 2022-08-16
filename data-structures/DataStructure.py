4cca0d293249476bfb609106c3f38ffb00dffb20
programmercave0 <mailabhi593@gmail.com>
2022-08-16 17:27:07 +0530

    Stack added in DataStructure

--- data-structures/DataStructure.py
+++ data-structures/DataStructure.py
@@ -35,3 +35,44 @@ class Queue:
 			print(self.__elements[index])
 
 
+"""************   Stack     ***********"""
+
+class Stack:
+	def __init__(self, max_size):
+		self.__max_size = max_size
+		self.__elements = [None] * self.__max_size
+		self.__top = -1
+
+	def is_full(self):
+		if (self.__top == self.__max_size - 1):
+			return True
+		return False
+
+	def is_empty(self):
+		if (self.__top == -1):
+			return True 
+		return False
+
+	def push(self, data):
+		if (self.is_full()):
+			print("This stack is full!!")
+		else:
+			self.__top += 1
+			self.__elements[self.__top] = data
+
+	def pop(self):
+		if (self.is_empty()):
+			print("The stack is empty!!")
+		else:
+			data = self.__elements[self.__top]
+			self.__top -= 1
+			return data
+
+	def display(self):
+		if (self.is_empty()):
+			print("The stack is empty!!")
+		else:
+			index = self.__top
+			while (index >= 0):
+				print(self.__elements[index])
+				index += 1
