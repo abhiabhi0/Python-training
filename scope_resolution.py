3b729a9006e7fe03aa40d5b947c0a1316187d260
programmercave0 <mailabhi593@gmail.com>
2022-08-24 19:10:15 +0530

    scope resolution

---
+++ scope_resolution.py
@@ -0,0 +1,17 @@
+temp = 10   # global-scope variable
+def func():
+     temp = 20   # local-scope variable
+     print(temp)
+print(temp)   # output => 10
+func()    # output => 20
+print(temp)   # output => 10
+
+
+temp = 10   # global-scope variable
+def func():
+     global temp
+     temp = 20   # local-scope variable
+     print(temp)
+print(temp)   # output => 10
+func()    # output => 20
+print(temp)   # output => 20
